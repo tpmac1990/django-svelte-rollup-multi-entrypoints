88b6ccc13185246c620aa77b363e66e491c3f05f
tpmac1990 <terence@interaction.net.au>
2022-09-14 10:59:54 +1000

    added common app to tailwind watch

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: [
     // "./mysite/polls/src/*.{html,js,svelte}",
-    "./mysite/other/src/*.{html,js,svelte}",
-    "./mysite/sform/src/*.{html,js,svelte}",
+    "./mysite/other/**/*.{html,js,svelte}",
+    "./mysite/sform/**/*.{html,js,svelte}",
     "./mysite/polls/**/*.{html,js,svelte}",
     "./mysite/htmx_todo/**/*.{html,js,svelte}",
+    "./mysite/common/**/*.{html,js,svelte}",
     // Error: both lines below cause an infinite reload loop
     // "./mysite/**/*.{html,js,svelte}",
     // "./mysite/*/src/*.svelte"
