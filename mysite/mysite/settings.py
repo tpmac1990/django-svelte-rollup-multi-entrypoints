9e36586b3e4c6ab540127d658162a1812ddd6197
tpmac1990 <terence@interaction.net.au>
2022-11-07 07:58:26 +1100

    manage media files locally

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -150,13 +150,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "svelte", "public"), 
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media"),
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media"),
+
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 
 AUTH_USER_MODEL = 'registration.User'
