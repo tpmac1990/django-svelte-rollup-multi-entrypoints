913d67a372820d07ebfa601f69b1f8440cc265dc
tpmac1990 <terence@interaction.net.au>
2022-11-08 07:48:49 +1100

    use s3 for media files in production

--- mysite/htmx_fragments/models.py
+++ mysite/htmx_fragments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 
 class Image(models.Model):
@@ -7,3 +8,8 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+# delete the image from s3 bucket
+@receiver(models.signals.post_delete, sender=Image)
+def remove_file_from_s3(sender, instance, using, **kwargs):
+    instance.image.delete(save=False)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -179,3 +179,24 @@ INTERNAL_IPS = [
 
 CELERY_BROKER_URL = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://redis:6379"
+
+
+if not DEBUG:
+    # store media files in s3 bucket
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
+
+    # # Store static files in s3
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    # AWS_S3_REGION_NAME = "ap-southeast-2"
+    # AWS_S3_CUSTOM_DOMAIN = (
+    #     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+    # )
+    # AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}"
+    # AWS_LOCATION = "static"
+    # STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/"
