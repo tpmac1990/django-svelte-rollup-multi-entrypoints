6e6cb6f760aab0c39553f8951191578df797c015
tpmac1990 <terence@interaction.net.au>
2022-11-15 20:45:07 +1100

    minimise css for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    production && require("cssnano"),
+    // Brotli // for compression
+    // npm install --save-dev rollup-plugin-brotli
   ],
 }
